76162503b2e608bb627a01a3b3c2e46c41e49547
tuananh <tuanan.devcandy@gmail.com>
2024-08-17 00:13:01 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello world
+      <Button>Test</Button>
+    </main>
+  );
+}
